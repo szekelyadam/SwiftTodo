6025d8fba52a10ccc68fbe1c7f389a2addae1fa0
Ádibádi <adibadi@Richards-MacBook-Pro.local>
2015-11-28 18:16:59 +0100

    initial commit

---
+++ OB/ToDoItem.swift
@@ -0,0 +1,12 @@
+//
+//  ToDoItem.swift
+//  OB
+//
+//  Created by Ádibádi on 28/11/15.
+//  Copyright © 2015 Székely Ádám. All rights reserved.
+//
+
+import UIKit
+
+class ToDoItem: NSObject {
+}
---
+++ OB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OB
+//
+//  Created by Ádibádi on 28/11/15.
+//  Copyright © 2015 Székely Ádám. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableVies: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
