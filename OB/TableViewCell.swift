d3c45f1b349208b65f0785692ead59833b35758f
Ádibádi <adibadi@Richards-MacBook-Pro.local>
2015-11-28 22:38:37 +0100

    color change

--- OB/TableViewCell.swift
+++ OB/TableViewCell.swift
@@ -12,6 +12,8 @@ import QuartzCore
 class TableViewCell: UITableViewCell {
 
     let gradientLayer = CAGradientLayer()
+    var originalCenter = CGPoint()
+    var deleteOnDragRelease = false
     
     required init(coder aDecoder: NSCoder) {
         fatalError("NSCoding not supported")
@@ -29,10 +31,49 @@ class TableViewCell: UITableViewCell {
         gradientLayer.colors = [color1, color2, color3, color4]
         gradientLayer.locations = [0.0, 0.01, 0.95, 1.0]
         layer.insertSublayer(gradientLayer, atIndex: 0)
+        
+        // add a pan recognizer
+        var recognizer = UIPanGestureRecognizer(target: self, action: "handlePan:")
+        recognizer.delegate = self
+        addGestureRecognizer(recognizer)
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
         gradientLayer.frame = bounds
+    }
+    
+    func handlePan(recognizer: UIPanGestureRecognizer) {
+        if recognizer.state == .Began {
+            // when the gesture begins, record the current center location
+            originalCenter = center
+        }
+        
+        if recognizer.state == .Changed {
+            let translation = recognizer.translationInView(self)
+            center = CGPointMake(originalCenter.x + translation.x, originalCenter.y)
+            // has the user dragged the item far enough the initiate a delete/complete?
+            deleteOnDragRelease = frame.origin.x < -frame.size.width / 2.0
+        }
+        
+        if recognizer.state == .Ended {
+            // the frame this cell had before user dragged it
+            let originalFrame = CGRect(x: 0, y: frame.origin.y, width: bounds.size.width, height: bounds.size.height)
+            if !deleteOnDragRelease {
+                // if the item is not being deleted, snap back to the original location
+                UIView.animateWithDuration(0.2, animations: {self.frame = originalFrame})
+            }
+        }
+    }
+    
+    override func gestureRecognizerShouldBegin(gestureRecognizer: UIGestureRecognizer) -> Bool {
+        if let panGestureRecognizer = gestureRecognizer as? UIPanGestureRecognizer {
+            let translation = panGestureRecognizer.translationInView(superview!)
+            if fabs(translation.x) > fabs(translation.y) {
+                return true
+            }
+            return false
+        }
+        return false
     }
 }
--- OB/ViewController.swift
+++ OB/ViewController.swift
@@ -30,6 +30,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         toDoItems.append(ToDoItem(text: "make peace"))
         
         tableView.separatorStyle = .None
+        tableView.backgroundColor = UIColor.whiteColor()
         tableView.rowHeight = 50.0
     }
     
@@ -45,17 +46,19 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! TableViewCell
         let item = toDoItems[indexPath.row]
         cell.textLabel?.text = item.text
+        cell.selectionStyle = .None
         return cell
     }
     
-    func colorForIndex(index: Int) -> UIColor {
-        let itemCount = toDoItems.count - 1
-        let val = (CGFloat(index) / CGFloat(itemCount)) * 0.6
-        return UIColor(red: 1.0, green: val, blue: 0.0, alpha: 1.0)
-    }
+//    func colorForIndex(index: Int) -> UIColor {
+//        let itemCount = toDoItems.count - 1
+//        let val = (CGFloat(index) / CGFloat(itemCount)) * 0.6
+//        return UIColor(red: 1.0, green: val, blue: 0.0, alpha: 1.0)
+//    }
     
     func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
-        cell.backgroundColor = colorForIndex(indexPath.row)
+        cell.backgroundColor = UIColor(red: 200/255, green: 227/255, blue: 191/255, alpha: 1.0)
+        cell.textLabel?.textColor = UIColor.whiteColor()
     }
 }
 
